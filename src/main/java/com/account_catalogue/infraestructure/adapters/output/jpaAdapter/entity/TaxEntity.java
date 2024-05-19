2265f39f91693eca2077f992bd2b5816918b79a8
Brayan Majin Gaviria <brayannmajin@gmail.com>
2024-05-18 23:55:51 -0500

    add entety Tax

---
+++ src/main/java/com/account_catalogue/infraestructure/adapters/output/jpaAdapter/entity/TaxEntity.java
@@ -0,0 +1,11 @@
+package com.account_catalogue.infraestructure.adapters.output.jpaAdapter.entity;
+@Entity
+
+@Builder
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Table(name="Tax")
+public class TaxEntity {
+    
+}
